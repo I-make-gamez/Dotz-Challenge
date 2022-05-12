34ae85a1b26f2ebd049da574f965ccaccc098302
I-Make-Gamez <ebelcher143@gmail.com>
2022-05-12 13:56:08 -0400

    Initial commit

---
+++ blob.js
@@ -0,0 +1,47 @@
+var en = 0
+var timer = 60
+var posib = true
+const eatenList = document.querySelector('.el')
+const tim = document.querySelector('.tim')
+
+function Blob(x, y, r){
+    this.pos = createVector(x, y)
+    this.r = r
+
+    this.update = function(){
+        var mouse = createVector(mouseX-width/2, mouseY-height/2)
+        mouse.setMag(3)
+        this.pos.add(mouse)
+    }
+
+    this.eats = function(other){
+        var d = p5.Vector.dist(this.pos, other.pos)
+        if(d < this.r + other.r){
+            this.r += other.r*0.01
+            en += 1
+            eatenList.innerHTML="Total Eaten: " + en
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    this.show = function(){
+        fill(255)
+        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+    }
+
+    this.updateTimer = function(){
+        setInterval(function(){
+            if(posib === true){
+                timer -= 1
+                tim.innerHTML = `Timer: ${timer}`
+                if(timer === 0){
+                noLoop()
+                posib = false
+            }
+            }
+        }, 1000)        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var blob;
+
+var blobs = [];
+
+const sbtn = document.querySelector('.sbtn')
+
+function setup(){
+    createCanvas(480, 550)
+    blob = new Blob(width/2, height/2, 32)
+    for(var i=0; i<1500; i++){
+        var x = random(-width,width*4)
+        var y = random(-height,height*4)
+        var r = random(5, 10)
+        blobs[i] = new Blob(x, y, r)
+    }
+    noLoop()
+}
+
+function draw(){
+    background(0)
+    translate(width/2-blob.pos.x, height/2-blob.pos.y)
+    for(var i=blobs.length-1; i>=0; i--){
+        if(blob.eats(blobs[i])){
+            blobs.splice(i, 1)
+
+        }
+        blobs[i].show()
+    }
+    blob.show();
+    blob.update();
+}
+
+sbtn.addEventListener('click', function(){
+    sbtn.disabled = true
+    loop()
+    blob.updateTimer();
+})
